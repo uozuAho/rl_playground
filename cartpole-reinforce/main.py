090b6f8b77b0c5777cb05f98f47c6bc718c52f6b
me <me@me.com>
2025-02-10 16:26:46 +1100

    docs

--- cartpole-reinforce/main.py
+++ cartpole-reinforce/main.py
@@ -95,43 +95,47 @@ def reinforce(policy, optimizer, n_training_episodes, max_t, gamma, print_every)
 
         returns = deque(maxlen=max_t)
         n_steps = len(rewards)
-        # Compute the discounted returns at each timestep,
-        # as
-        #      the sum of the gamma-discounted return at time t (G_t) + the reward at time t
+        # Compute the discounted returns at each timestep, as the sum of the
+        # gamma-discounted return at time t (G_t) + the reward at time t
         #
-        # In O(N) time, where N is the number of time steps
-        # (this definition of the discounted return G_t follows the definition of this quantity
+        # In O(N) time, where N is the number of time steps (this definition of
+        # the discounted return G_t follows the definition of this quantity
         # shown at page 44 of Sutton&Barto 2017 2nd draft)
-        # G_t = r_(t+1) + r_(t+2) + ...
+        #
+        #    G_t = r_(t+1) + r_(t+2) + ...
 
         # Given this formulation, the returns at each timestep t can be computed
-        # by re-using the computed future returns G_(t+1) to compute the current return G_t
-        # G_t = r_(t+1) + gamma*G_(t+1)
-        # G_(t-1) = r_t + gamma* G_t
-        # (this follows a dynamic programming approach, with which we memorize solutions in order
-        # to avoid computing them multiple times)
+        # by re-using the computed future returns G_(t+1) to compute the current
+        # return G_t
+        #   G_t = r_(t+1) + gamma*G_(t+1)
+        #   G_(t-1) = r_t + gamma* G_t
+        # (this follows a dynamic programming approach, with which we memorize
+        # solutions in order to avoid computing them multiple times)
 
-        # This is correct since the above is equivalent to (see also page 46 of Sutton&Barto 2017 2nd draft)
-        # G_(t-1) = r_t + gamma*r_(t+1) + gamma*gamma*r_(t+2) + ...
+        # This is correct since the above is equivalent to (see also page 46 of
+        # Sutton&Barto 2017 2nd draft)
+        #   G_(t-1) = r_t + gamma*r_(t+1) + gamma*gamma*r_(t+2) + ...
 
-        ## Given the above, we calculate the returns at timestep t as:
+        # Given the above, we calculate the returns at timestep t as:
         #               gamma[t] * return[t] + reward[t]
         #
-        ## We compute this starting from the last timestep to the first, in order
-        ## to employ the formula presented above and avoid redundant computations that would be needed
-        ## if we were to do it from first to last.
+        # We compute this starting from the last timestep to the first, in order
+        # to employ the formula presented above and avoid redundant computations
+        # that would be needed if we were to do it from first to last.
 
-        ## Hence, the queue "returns" will hold the returns in chronological order, from t=0 to t=n_steps
-        ## thanks to the appendleft() function which allows to append to the position 0 in constant time O(1)
-        ## a normal python list would instead require O(N) to do this.
+        # Hence, the queue "returns" will hold the returns in chronological
+        # order, from t=0 to t=n_steps thanks to the appendleft() function which
+        # allows to append to the position 0 in constant time O(1) a normal
+        # python list would instead require O(N) to do this.
         for t in range(n_steps)[::-1]:
             disc_return_t = returns[0] if len(returns) > 0 else 0
             returns.appendleft(gamma * disc_return_t + rewards[t])
 
-        ## standardization of the returns is employed to make training more stable
+        # standardization of the returns is employed to make training more
+        # stable
         eps = np.finfo(np.float32).eps.item()
-        ## eps is the smallest representable float, which is
-        # added to the standard deviation of the returns to avoid numerical instabilities
+        # eps is the smallest representable float, which is added to the
+        # standard deviation of the returns to avoid numerical instabilities
         returns = torch.tensor(returns)
         returns = (returns - returns.mean()) / (returns.std() + eps)
 
