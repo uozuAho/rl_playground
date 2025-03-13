68288a4bd442efc640aca927b8d8b4b9370db85b
me <me@me.com>
2025-03-13 16:31:12 +1100

    extract agents.compare

---
+++ ttt/ttt/agents/compare.py
@@ -0,0 +1,51 @@
+from collections import Counter
+import typing as t
+
+from ttt.agents.agent import TttAgent
+import ttt.env
+from ttt.env import TicTacToeEnv
+
+
+type Result = t.Literal['O', 'X', 'draw']
+
+
+def play_game(agent_x: TttAgent, agent_o: TttAgent) -> Result:
+    """ Returns winner: X, O or draw """
+    game = TicTacToeEnv()
+    done = False
+    while not done:
+        if game.current_player == 'X':
+            move = agent_x.get_action(game)
+        else:
+            move = agent_o.get_action(game)
+        _, _, done, _, _ = game.step(move)
+    status = game.get_status()
+    if status == ttt.env.X_WIN:
+        return 'X'
+    elif status == ttt.env.O_WIN:
+        return 'O'
+    return 'draw'
+
+
+def play_games(agent_x: TttAgent, agent_o: TttAgent, n_games: int) -> t.Tuple[int, int, int]:
+    """ Returns counts: x wins, o wins, draws """
+    ctr = Counter()
+    for i in range(n_games):
+        winner = play_game(agent_x, agent_o)
+        ctr[winner] += 1
+    return ctr['X'], ctr['O'], ctr['draw']
+
+
+def play_and_report(
+        agent_x: TttAgent,
+        label_x: str,
+        agent_o: TttAgent,
+        label_o: str,
+        n_games: int):
+    """ Plays N games and prints the results """
+    x, o, d = play_games(agent_x, agent_o, n_games)
+    xpc = 100 * x / n_games
+    opc = 100 * o / n_games
+    print(f'{label_x} (x) vs {label_o} (o). {n_games} games. ' +
+          f'x wins: {x} ({xpc:.1f}%), o wins: {o} ({opc:.1f}%). draws: {d}')
+
