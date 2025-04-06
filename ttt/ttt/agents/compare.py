c4e60e8a431d1abcb3919983acf3280c8aa7a313
me <me@me.com>
2025-04-06 15:37:08 +1000

    ttt: better compare2

--- ttt/ttt/agents/compare.py
+++ ttt/ttt/agents/compare.py
@@ -71,15 +71,20 @@ def play_and_report(
 
 
 def play_game2(agent_x: TttAgent2, agent_o: TttAgent2) -> GameResult:
-    """ Returns winner: X, O or draw """
-    env = env2.Env()
+    game = env2.Env()
     done = False
     while not done:
-        if env.current_player == env2.X:
-            move = agent_x.get_action(env)
+        if game.current_player == env2.X:
+            move = agent_x.get_action(game)
         else:
-            move = agent_o.get_action(env)
-        _, reward, done, _, _ = env.step(move)
+            move = agent_o.get_action(game)
+        try:
+            _, reward, done, _, _ = game.step(move)
+        except IllegalActionError:
+            if game.current_player == env2.X:
+                return 'X-illegal'
+            else:
+                return 'O-illegal'
     if reward == 1:
         return 'X'
     elif reward == -1:
@@ -87,13 +92,12 @@ def play_game2(agent_x: TttAgent2, agent_o: TttAgent2) -> GameResult:
     return 'draw'
 
 
-def play_games2(agent_x: TttAgent2, agent_o: TttAgent2, n_games: int) -> t.Tuple[int, int, int]:
-    """ Returns counts: x wins, o wins, draws """
+def play_games2(agent_x: TttAgent2, agent_o: TttAgent2, n_games: int):
     ctr: Counter[GameResult] = Counter()
     for _ in range(n_games):
-        winner = play_game2(agent_x, agent_o)
-        ctr[winner] += 1
-    return ctr['X'], ctr['O'], ctr['draw']
+        result = play_game2(agent_x, agent_o)
+        ctr[result] += 1
+    return ctr
 
 
 def play_and_report2(
@@ -101,10 +105,23 @@ def play_and_report2(
         label_x: str,
         agent_o: TttAgent2,
         label_o: str,
-        n_games: int):
-    """ Plays N games and prints the results """
-    x, o, d = play_games2(agent_x, agent_o, n_games)
-    xpc = 100 * x / n_games
-    opc = 100 * o / n_games
-    print(f'{label_x} (x) vs {label_o} (o). {n_games} games. ' +
-          f'x wins: {x} ({xpc:.1f}%), o wins: {o} ({opc:.1f}%). draws: {d}')
+        n_games: int,
+        quiet: bool = False):
+    start = time.time()
+    results = play_games2(agent_x, agent_o, n_games)
+    end = time.time()
+
+    x, o, d = results['X'], results['O'], results['draw']
+    xi, oi = results['X-illegal'], results['O-illegal']
+    xpc = 100 * results['X'] / n_games
+    opc = 100 * results['O'] / n_games
+    dpc = 100 * results['draw'] / n_games
+    xipc = 100 * results['X-illegal'] / n_games
+    oipc = 100 * results['O-illegal'] / n_games
+
+    msg = (f'{label_x:<16} (x) vs {label_o:<16} (o). {n_games} games in {end-start:.1f}s. ' +
+          f'x wins: {x:>3} ({xpc:>5.1f}%), o wins: {o:>3} ({opc:>5.1f}%), draws: {d:>3} ({dpc:>5.1f}%), ' +
+          f'x illegal: {xi:>3} ({xipc:>5.1f}%), o illegal: {oi:>3} ({oipc:>5.1f}%)')
+    if not quiet:
+        print(msg)
+    return msg
