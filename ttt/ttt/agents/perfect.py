94c9ebb599358c0a87b0eb323bff3d172437f10c
me <me@me.com>
2025-04-11 15:18:36 +1000

    ttt: fix perfect o loss

--- ttt/ttt/agents/perfect.py
+++ ttt/ttt/agents/perfect.py
@@ -14,21 +14,18 @@ def player_won(env: t3.Env, player: t3.Player):
 def find_best_move(env: t3.Env, player=t3.X):
     opponent = t3.other_player(player)
 
-    # Winning move
-    # hacking env internals, using env.copy/env.step is dangerous
+    # make a winning move
     for pos in range(9):
         if env.board[pos] == t3.EMPTY:
             tempboard = env.board[:]
             tempboard[pos] = player
-            # status = ttt.env.check_game_status(tempboard)
             status = t3.status(tempboard)
             if status == t3.O and player == t3.O:
                 return pos
             if status == t3.X and player == t3.X:
                 return pos
 
-    # Block opponent's win:
-    # hacking this for now. hard to do esp. with an opponent in place
+    # block opponent's win
     for pos in range(9):
         if env.board[pos] == t3.EMPTY:
             tempboard = env.board[:]
@@ -55,7 +52,7 @@ def find_best_move(env: t3.Env, player=t3.X):
             for a in env.valid_actions():
                 if a in [1, 3, 5, 7]:
                     return a
-        # todo: block another winning setup for x
+        # block another winning setup for x:
         # 1. x goes corner
         # 2. o goes middle
         # 3. x goes opposite side
@@ -78,6 +75,32 @@ def find_best_move(env: t3.Env, player=t3.X):
         if env.board[8] == x:
             if env.board[1] == x:
                 return 2
+            if env.board[3] == x:
+                return 6
+        # block yet another winning setup for x:
+        # 1. x goes side
+        # 2. o goes middle
+        # 3. x goes adjacent side
+        # 4. o should take any corner except the one opposite the corner between
+        #    the two x's
+        if env.board[1] == x:
+            if env.board[3] == x:
+                return 0
+            if env.board[5] == x:
+                return 2
+        if env.board[3] == x:
+            if env.board[1] == x:
+                return 0
+            if env.board[7] == x:
+                return 6
+        if env.board[5] == x:
+            if env.board[1] == x:
+                return 2
+            if env.board[7] == x:
+                return 8
+        if env.board[7] == x:
+            if env.board[5] == x:
+                return 8
             if env.board[3] == x:
                 return 6
 
