4322072eff7d3597c158f5a8600d71c3c589b761
me <me@me.com>
2025-04-15 21:01:01 +1000

    ttt: WIP: better nn arch

--- ttt/torch-nn-learn_tabular_vals.py
+++ ttt/torch-nn-learn_tabular_vals.py
@@ -23,15 +23,18 @@ from ttt.agents.tab_greedy_v import TabGreedyVAgent
 class Simple(nn.Module):
     """ Can't learn more than about 10 state values with accuracy """
     # loss: avg: 0.081, max: 0.748
-    def __init__(self):
+    def __init__(self, device):
         super(Simple, self).__init__()
+        self.device = device
         self.l1 = nn.Linear(9, 32, dtype=torch.float32)
         self.l2 = nn.Linear(32, 32, dtype=torch.float32)
         self.l3 = nn.Linear(32, 1, dtype=torch.float32)
         self.optimiser = optim.Adam(self.parameters(), lr=1e-4)
 
-    def forward(self, state_str: str):
-        x = self._state2input(state_str)
+    def print_summary(self, batch_size):
+        torchinfo.summary(self, input_size=(batch_size, 9))
+
+    def forward(self, x):
         x = self.l1(x)
         x = self.l2(x)
         x = self.l3(x)
@@ -49,16 +52,36 @@ class Simple(nn.Module):
 
         return loss.item()
 
+    def learn_batch(self, batch: t.List[t.Tuple[str, float]]):
+        states = [b[0] for b in batch]
+        vals = [b[1] for b in batch]
+        state_batch = self._states2batch(states)
+        q_est = net(state_batch)
+        q_actual = torch.tensor(vals, dtype=torch.float32).unsqueeze(0).t().to(DEVICE)
+
+        self.optimiser.zero_grad()
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(q_est, q_actual)
+        loss.backward()
+        self.optimiser.step()
+
+        return loss.item()
+
     def _state2input(self, state_str: str):
         return torch.tensor(state2nums(state_str), dtype=torch.float32)
+
+    def _states2batch(self, states: t.List[str]):
+        batch = torch.stack([self._state2input(s) for s in states]).to(self.device)
+        return batch
 
 
 class SimpleConv(nn.Module):
     # Based on https://github.com/arjangroen/RLC/blob/e54eb7380875f64fd06106c59aa376b426d9e5ca/RLC/real_chess/agent.py#L122C5-L133C29
     # similar perf to simple
     # loss: avg: 0.081, max: 0.645
-    def __init__(self):
+    def __init__(self, device):
         super(SimpleConv, self).__init__()
+        self.device = device
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, padding=0)
         self.activation = nn.Sigmoid()
         self.flatten = nn.Flatten()
@@ -66,8 +89,10 @@ class SimpleConv(nn.Module):
         self.fc2 = nn.Linear(10, 1)
         self.optimiser = optim.Adam(self.parameters(), lr=1e-4)
 
-    def forward(self, state_str: str):
-        x = self._state2input(state_str)
+    def print_summary(self, batch_size):
+        torchinfo.summary(self, input_size=(batch_size, 1, 3, 3))
+
+    def forward(self, x):
         x = self.activation(self.conv1(x))
         x = self.flatten(x)
         x = self.activation(self.fc1(x))
@@ -75,8 +100,24 @@ class SimpleConv(nn.Module):
         return x
 
     def learn_single(self, state: str, value: float):
-        q_est = net(state)
+        input_t = self._state2input(state)
+        q_est = net(input_t)
         q_actual = torch.tensor([value]).unsqueeze(0)
+
+        self.optimiser.zero_grad()
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(q_est, q_actual)
+        loss.backward()
+        self.optimiser.step()
+
+        return loss.item()
+
+    def learn_batch(self, batch: t.List[t.Tuple[str, float]]):
+        states = [b[0] for b in batch]
+        vals = [b[1] for b in batch]
+        state_batch = self._states2batch(states)
+        q_est = net(state_batch)
+        q_actual = torch.tensor(vals, dtype=torch.float32).unsqueeze(0).t().to(DEVICE)
 
         self.optimiser.zero_grad()
         criterion = nn.SmoothL1Loss()
@@ -93,6 +134,13 @@ class SimpleConv(nn.Module):
         i = i.unsqueeze(0) # batch of 1
         return i
 
+    def _states2batch(self, states: t.List[str]):
+        nums = [state2nums(s) for s in states]
+        # unsqueeze -> 1 channel for conv2d
+        ts = [torch.tensor(n, dtype=torch.float32).reshape((3, 3)).unsqueeze(0) for n in nums]
+        batch = torch.stack(ts).to(self.device)
+        return batch
+
 
 class MidConv(nn.Module):
     # Does better! Was still learning when I stopped it:
@@ -108,6 +156,9 @@ class MidConv(nn.Module):
         self.fc3 = nn.Linear(8, 1)
         self.optimiser = optim.Adam(self.parameters(), lr=1e-4)
 
+    def print_summary(self, batch_size):
+        torchinfo.summary(self, input_size=(batch_size, 1, 3, 3))
+
     def forward(self, x):
         x = torch.relu(self.conv1(x))
         x = torch.relu(self.conv2(x))
@@ -122,7 +173,7 @@ class MidConv(nn.Module):
         vals = [b[1] for b in batch]
         state_batch = self._states2batch(states)
         q_est = net(state_batch)
-        q_actual = torch.tensor(vals, dtype=torch.float32).unsqueeze(0).t().to(device)
+        q_actual = torch.tensor(vals, dtype=torch.float32).unsqueeze(0).t().to(DEVICE)
 
         self.optimiser.zero_grad()
         criterion = nn.SmoothL1Loss()
@@ -147,11 +198,37 @@ class MidConv(nn.Module):
         return batch
 
 
+class Conv3(nn.Module):
+    """WIP. """
+    def __init__(self, device):
+        super(Conv3, self).__init__()
+        self.device = device
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=2, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=2, padding=1)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(800, 32)
+        self.fc2 = nn.Linear(32, 8)
+        self.fc3 = nn.Linear(8, 1)
+        self.optimiser = optim.Adam(self.parameters(), lr=1e-4)
+
+    def print_summary(self, batch_size):
+        torchinfo.summary(self, input_size=(batch_size, 1, 3, 3))
+
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = self.flatten(x)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
 
 def state2nums(state_str: str):
     state_str = state_str.replace('|', '')
     assert len(state_str) == 9
-    return [2 if c == 'x' else 1 if c == 'o' else 0 for c in state_str]
+    return [1 if c == 'x' else -1 if c == 'o' else 0 for c in state_str]
 
 
 def batches(lst, size):
@@ -159,20 +236,26 @@ def batches(lst, size):
         yield lst[i:i + size]
 
 
-device = torch.device(
+DEVICE = torch.device(
     "cuda" if torch.cuda.is_available() else
     "mps" if torch.backends.mps.is_available() else
     "cpu"
 )
 # device = 'cpu'
-print(f'using device {device}')
+print(f'using device {DEVICE}')
 
-batch_size = 50
-net = MidConv(device).to(device)
-torchinfo.summary(net, input_size=(batch_size, 1, 3, 3))
+NUM_VALS_TO_LEARN = 999999999999 # set to 999999999999999 for max
+BATCH_SIZE = 100
+# net = Simple(DEVICE)
+# net = SimpleConv(DEVICE)
+net = MidConv(DEVICE).to(DEVICE)
+net.print_summary(BATCH_SIZE)
 tab_agent = TabGreedyVAgent.load('trained_models/tabgreedyv-rng')
 q_table = tab_agent._q_table
-all_vals = list(q_table.values())
+all_vals = list(q_table.values())[:NUM_VALS_TO_LEARN]
+
+print(f'Fitting net to {len(all_vals)} values. Batch size: {BATCH_SIZE}.')
+input("press any key to start, ctrl+c to stop")
 
 start = time.time()
 t_prev = time.time()
@@ -180,14 +263,7 @@ for i in range(99999999999999999):
     random.shuffle(all_vals)
     losses = []
 
-    # todo: support learn_batch in all models
-    # update net for every state,value:
-    # for state, value in all_vals:
-    #     loss = net.learn_single(state, value)
-    #     losses.append(loss)
-
-    # update net in batches
-    for batch in batches(all_vals, batch_size):
+    for batch in batches(all_vals, BATCH_SIZE):
         loss = net.learn_batch(batch)
         losses.append(loss)
 
