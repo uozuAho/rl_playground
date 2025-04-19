c29e781237b15a1fe271905d61882899c734ea0e
me <me@me.com>
2025-04-19 15:00:50 +1000

    replace nnmcts with mcts(nn)

--- ttt/bot-showdown.py
+++ ttt/bot-showdown.py
@@ -7,7 +7,6 @@ import typing as t
 from ttt.agents.agent import TttAgent
 from ttt.agents.compare import play_and_report
 from ttt.agents.mcts import MctsAgent
-from ttt.agents.mcts import MctsAgent
 from ttt.agents.perfect import PerfectAgent
 from ttt.agents.qlearn import TabQlearnAgent
 from ttt.agents.random import RandomAgent
@@ -17,7 +16,6 @@ from ttt.agents.sb3_maskppo import Sb3MaskPpoAgent
 from ttt.agents.sb3ppo import Sb3PpoAgent
 from ttt.agents.tab_greedy_v import TabGreedyVAgent
 from ttt.agents.torch_nn_greedy_v import NnGreedyVAgent
-from ttt.agents.torch_nn_greedy_v_mcts import NnGreedyVMctsAgent
 from utils.torch_device import find_device
 
 
@@ -37,27 +35,27 @@ def main():
         # (MctsAgent(n_sims=1), "mcts1"),
         # (MctsAgent(n_sims=5), "mcts5"),
         (MctsAgent(n_sims=10), "mcts10"),
-        # (PerfectAgent(), "perfect"),
+        (PerfectAgent(), "perfect"),
     ]
 
-    # load_or_train_agent(agents, 'tabsarsa-rng', TabSarsaAgent,
-    #     lambda: TabSarsaAgent.train_new(RandomAgent(), 100 if TRAIN_FAST else 5000))
-    # load_or_train_agent(agents, 'tabqlearn-rng', TabQlearnAgent,
-    #     lambda: TabQlearnAgent.train_new(RandomAgent(), 100 if TRAIN_FAST else 5000))
-    # load_or_train_agent(agents, 'tabgreedyv-rng', TabGreedyVAgent,
-    #     lambda: TabGreedyVAgent.train_new(100 if TRAIN_FAST else 5000))
-    # load_or_train_agent(agents, 'sb3dqn-rng', Sb3DqnAgent,
-    #     lambda: Sb3DqnAgent.train_new(opponent=RandomAgent(), steps=100 if TRAIN_FAST else 50000, verbose=VERBOSE))
-    # load_or_train_agent(agents, 'sb3ppo-rng', Sb3PpoAgent,
-    #     lambda: Sb3PpoAgent.train_new(opponent=RandomAgent(), steps=100 if TRAIN_FAST else 100000, verbose=VERBOSE))
-    # load_or_train_agent(agents, 'sb3maskppo-rng', Sb3MaskPpoAgent,
-    #     lambda: Sb3MaskPpoAgent.train_new(opponent=RandomAgent(), steps=100 if TRAIN_FAST else 50000, verbose=VERBOSE))
-    # load_or_train_agent(agents, 'nngreedyv-rng', NnGreedyVAgent,
-    #     lambda: NnGreedyVAgent.train_new(RandomAgent(), 100 if TRAIN_FAST else 5000, DEVICE), DEVICE)
-    # load_or_train_agent(agents, 'nngreedymcts10', None, None, DEVICE,
-    #     lambda: load_NnGreedyVMctsAgent('nngreedyv-rng', 10))
-    # load_or_train_agent(agents, 'nngreedymcts10', None, None, DEVICE,
-    #     lambda: load_NnGreedyVMctsAgent('nngreedyv-rng', 20))
+    load_or_train_agent(agents, 'tabsarsa-rng', TabSarsaAgent,
+        lambda: TabSarsaAgent.train_new(RandomAgent(), 100 if TRAIN_FAST else 5000))
+    load_or_train_agent(agents, 'tabqlearn-rng', TabQlearnAgent,
+        lambda: TabQlearnAgent.train_new(RandomAgent(), 100 if TRAIN_FAST else 5000))
+    load_or_train_agent(agents, 'tabgreedyv-rng', TabGreedyVAgent,
+        lambda: TabGreedyVAgent.train_new(100 if TRAIN_FAST else 5000))
+    load_or_train_agent(agents, 'sb3dqn-rng', Sb3DqnAgent,
+        lambda: Sb3DqnAgent.train_new(opponent=RandomAgent(), steps=100 if TRAIN_FAST else 50000, verbose=VERBOSE))
+    load_or_train_agent(agents, 'sb3ppo-rng', Sb3PpoAgent,
+        lambda: Sb3PpoAgent.train_new(opponent=RandomAgent(), steps=100 if TRAIN_FAST else 100000, verbose=VERBOSE))
+    load_or_train_agent(agents, 'sb3maskppo-rng', Sb3MaskPpoAgent,
+        lambda: Sb3MaskPpoAgent.train_new(opponent=RandomAgent(), steps=100 if TRAIN_FAST else 50000, verbose=VERBOSE))
+    load_or_train_agent(agents, 'nngreedyv-rng', NnGreedyVAgent,
+        lambda: NnGreedyVAgent.train_new(RandomAgent(), 100 if TRAIN_FAST else 5000, DEVICE), DEVICE)
+    load_or_train_agent(agents, 'nngreedymcts10', None, None, DEVICE,
+        lambda: load_NnGreedyVMctsAgent('nngreedyv-rng', 10))
+    load_or_train_agent(agents, 'nngreedymcts10', None, None, DEVICE,
+        lambda: load_NnGreedyVMctsAgent('nngreedyv-rng', 20))
 
     for a1, l1 in agents:
         for a2, l2 in agents:
@@ -107,10 +105,9 @@ def try_load(agent_class, path, device=None):
 
 def load_NnGreedyVMctsAgent(name, n_sims):
     path = TRAINED_MODELS_PATH/name
-    agent = try_load(NnGreedyVMctsAgent, path, DEVICE)
+    agent = try_load(NnGreedyVAgent, path, DEVICE)
     if agent:
-        agent.n_simulations = n_sims
-        return agent
+        return MctsAgent(n_sims, lambda e,p: agent.board_val(e.board) * p)
 
 
 if __name__ == "__main__":
