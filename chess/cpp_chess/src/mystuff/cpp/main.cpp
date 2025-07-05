69131dbc542f654dcae68fae3adef27d9a25e56c
me <me@me.com>
2025-07-05 13:27:45 +1000

    inline game

--- chess/cpp_chess/src/mystuff/cpp/main.cpp
+++ chess/cpp_chess/src/mystuff/cpp/main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
-#include "game.h"
 #include "agent_random.h"
 #include "chess/board.h"
 
 int main() {
     lczero::InitializeMagicBitboards();
 
-    RandomAgent white;
-    RandomAgent black;
-    Game game(&white, &black);
-    game.play();
-    std::cout << "Result: " << game.result() << std::endl;
+    RandomAgent white = RandomAgent();
+    RandomAgent black = RandomAgent();
+    Agent* agents[2] = {&white, &black};
+    LeelaBoardWrapper board = LeelaBoardWrapper();
+    int turn = 0;
+    while (!board.is_game_over()) {
+        auto move = agents[turn]->select_move(board);
+        board.make_move(move);
+        turn = 1 - turn;
+    }
+    std::cout << "Result: " << board.result() << std::endl;
     return 0;
 }
