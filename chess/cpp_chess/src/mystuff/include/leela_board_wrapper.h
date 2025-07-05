7bc41aa3d7f18fe36370f9a5b313bb3e05331c28
me <me@me.com>
2025-07-05 12:03:46 +1000

    prep for copilot

--- chess/cpp_chess/src/mystuff/include/leela_board_wrapper.h
+++ chess/cpp_chess/src/mystuff/include/leela_board_wrapper.h
@@ -7,13 +7,8 @@
 
 class LeelaBoardWrapper {
 public:
-    LeelaBoardWrapper();
-    void reset();
     void make_move(const lczero::Move& move);
-    const lczero::ChessBoard& board() const;
     bool is_game_over() const;
     std::string result() const;
     std::vector<lczero::Move> legal_moves() const;
-private:
-    lczero::ChessBoard* board_;
 };
