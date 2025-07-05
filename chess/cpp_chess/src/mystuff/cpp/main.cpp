b340847e88c6ccc7b59b9cd7e15e2bd66d22191d
me <me@me.com>
2025-07-05 12:35:31 +1000

    fix segfault: init bitboards

--- chess/cpp_chess/src/mystuff/cpp/main.cpp
+++ chess/cpp_chess/src/mystuff/cpp/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "game.h"
 #include "agent_random.h"
+#include "chess/board.h"
 
 int main() {
+    lczero::InitializeMagicBitboards();
+
     RandomAgent white;
     RandomAgent black;
     Game game(&white, &black);
