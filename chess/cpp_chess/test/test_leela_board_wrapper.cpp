0e56ee24cedc59086742f542970a17279c03891c
me <me@me.com>
2025-07-06 18:55:38 +1000

    ok i'm getting squares

--- chess/cpp_chess/test/test_leela_board_wrapper.cpp
+++ chess/cpp_chess/test/test_leela_board_wrapper.cpp
@@ -17,6 +17,15 @@ TEST_F(LeelaBoardWrapperTest, InitialState) {
     EXPECT_FALSE(moves.empty());
 }
 
+TEST_F(LeelaBoardWrapperTest, HowDoSquaresWork) {
+    LeelaBoardWrapper board;
+    const auto square_0 = lczero::Square::FromIdx(0);
+    const auto square_a1 = lczero::Square::Parse("a1");
+    EXPECT_EQ(square_0, square_a1);
+    EXPECT_EQ(board.piece_at(square_a1), lczero::kRook);
+    EXPECT_EQ(board.piece_at(lczero::Square::Parse("a2")), lczero::kPawn);
+}
+
 TEST_F(LeelaBoardWrapperTest, PlayFullGame) {
     LeelaBoardWrapper board;
     RandomAgent white;
