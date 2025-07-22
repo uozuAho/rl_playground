269907f5adb48501cde227b51def74e3ddd3bb09
me <me@me.com>
2025-07-22 19:57:09 +1000

    WIP: fixing value network trainer code

--- chess/csharp_chess/cschess.game/CodingAdventureChessGame.cs
+++ chess/csharp_chess/cschess.game/CodingAdventureChessGame.cs
@@ -2,6 +2,10 @@ using Chess.Core;
 
 namespace cschess.game;
 
+/// <summary>
+/// Wraps the coding adventure impl in an easy-to-use wrapper, while trying
+/// to keep it fast.
+/// </summary>
 public class CodingAdventureChessGame
 {
     private readonly Board _board;
@@ -10,6 +14,11 @@ public class CodingAdventureChessGame
     public static CodingAdventureChessGame StandardGame()
     {
         return new CodingAdventureChessGame(Board.CreateBoard());
+    }
+
+    public static CodingAdventureChessGame FromFen(string fen)
+    {
+        return new CodingAdventureChessGame(Board.CreateBoard(fen));
     }
 
     private CodingAdventureChessGame(Board board)
@@ -25,13 +34,53 @@ public class CodingAdventureChessGame
 
     public IEnumerable<Move> LegalMoves()
     {
-        // todo: perf: method group, linq
         return _moveGenerator.GenerateMoves(_board).ToArray().Select(Move.From);
     }
 
     public void MakeMove(Move move)
     {
         _board.MakeMove(move.CoreMove);
+    }
+
+    public PieceType? PieceAt(int square)
+    {
+        var pieceInt = _board.Square[square];
+
+        return Piece.PieceType(pieceInt) switch
+        {
+            Piece.None => null,
+            Piece.Pawn => PieceType.Pawn,
+            Piece.Knight => PieceType.Knight,
+            Piece.Bishop => PieceType.Bishop,
+            Piece.Rook => PieceType.Rook,
+            Piece.Queen => PieceType.Queen,
+            Piece.King => PieceType.King,
+            _ => throw new ArgumentOutOfRangeException(nameof(square), square, null)
+        };
+    }
+
+    public Color ColorAt(int square)
+    {
+        if (!PieceAt(square).HasValue) return Color.None;
+
+        var pieceInt = _board.Square[square];
+
+        return Piece.PieceColour(pieceInt) switch
+        {
+            Piece.White => Color.White,
+            Piece.Black => Color.Black,
+            _ => throw new ArgumentOutOfRangeException(nameof(square), square, null)
+        };
+    }
+
+    public int FullmoveCount()
+    {
+        return _board.PlyCount / 2;
+    }
+
+    public Color Turn()
+    {
+        return _board.IsWhiteToMove ? Color.White : Color.Black;
     }
 }
 
@@ -48,4 +97,21 @@ public class Move
     {
         CoreMove = coreMove;
     }
+}
+
+public enum PieceType
+{
+    Pawn = 1,
+    Rook = 2,
+    Bishop = 3,
+    Knight = 4,
+    Queen = 5,
+    King = 6,
+};
+
+public enum Color
+{
+    Black = -1,
+    None = 0,
+    White = 1,
 }
