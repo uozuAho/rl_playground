fix softmax

connect4/src/agents/az_nets.py
@@ -29,14 +29,14 @@ class ResNet(AzNet):
         # unsqueeze to batch of 1
         minput = enc_state.unsqueeze(0).to(self.device)
         plogits, val = self._model(minput)
-        # todo softmax
-        return plogits.squeeze().tolist(), val.item()
+        return plogits.softmax(dim=1).squeeze().tolist(), val.item()
 
     def pv_batch(self, states: list[c4.GameState]) -> list[types.PV]:
         minput = torch.stack([self._state2tensor(s) for s in states])
-        # todo softmax
         plogits, val = self._model(minput)
-        return list(zip(plogits.squeeze().tolist(), val.squeeze().tolist()))
+        return list(
+            zip(plogits.softmax(dim=1).squeeze().tolist(), val.squeeze().tolist())
+        )
 
     def _state2tensor(self, state: c4.GameState):
         board = state.board

connect4/src/test/agents/test_az_nets.py
@@ -2,6 +2,7 @@ import torch
 
 from agents import az_nets
 import env.connect4 as c4
+from utils.maths import is_prob_dist
 
 
 def test_resnet_pv():
@@ -10,7 +11,7 @@ def test_resnet_pv():
     with torch.no_grad():
         p, v = net.pv(state)
     assert len(p) == c4.ACTION_SIZE
-    assert all(type(x) is float for x in p)
+    assert is_prob_dist(p)
     assert type(v) is float
 
 
@@ -23,5 +24,5 @@ def test_resnet_pv_batch():
     assert len(pvs) == batch_size
     for p, v in pvs:
         assert len(p) == c4.ACTION_SIZE
-        assert all(type(x) is float for x in p)
+        assert is_prob_dist(p)
         assert type(v) is float