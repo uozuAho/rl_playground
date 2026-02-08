396b7cc1c978643353bc348c3ca08715e3e129f1
me <me@me.com>
2026-02-08 19:02:41 +1100

    bring all code to gpu exp

--- ttt/src/exp_ttt_gpu.py
+++ ttt/src/exp_ttt_gpu.py
@@ -5,10 +5,16 @@ import sys
 import time
 
 import numpy as np
+import torch
+from torch import nn
 from torch.optim import Adam
+import torch.nn.functional as F
 
-from agents.alphazero import _update_net, GameStep
+from agents.alphazero import GameStep
 from agents.az_nets import ResNet
+from utils.maths import is_prob_dist
+
+import ttt.env as t3
 
 
 def gen_game_step():
@@ -48,7 +54,7 @@ def runloop(device: str, net: ResNet, nsteps: int, optimiser: Adam, profile: boo
         total_gen_time += gen_time
 
         update_start = time.perf_counter()
-        _update_net(net, optimiser, steps, mask_invalid_actions=False, device=device)
+        update_net(net, optimiser, steps, mask_invalid_actions=False, device=device)
         update_time = time.perf_counter() - update_start
         total_update_time += update_time
         update_count += 1
@@ -76,6 +82,67 @@ def runloop(device: str, net: ResNet, nsteps: int, optimiser: Adam, profile: boo
 
         if profile and time.perf_counter() - start > 3:
             break
+
+
+def update_net(
+    model: nn.Module,
+    optimizer,
+    game_steps: list[GameStep],
+    mask_invalid_actions,
+    device,
+):
+    for s in game_steps:
+        assert is_prob_dist(s.mcts_probs)
+        for i, v in enumerate(s.valid_action_mask):
+            if not v:
+                assert s.board[i] != t3.EMPTY
+                assert s.mcts_probs[i] == 0
+    step_tuples = [s.as_tuple() for s in game_steps]
+    *_, policy_targets, value_targets = zip(*step_tuples)
+    state = torch.stack(
+        [board2tensor(board, player) for board, player, *_ in step_tuples]
+    ).to(device)
+
+    policy_targets, value_targets = (
+        np.array(policy_targets),
+        np.array(value_targets).reshape(-1, 1),
+    )
+
+    policy_targets = torch.tensor(policy_targets, dtype=torch.float32, device=device)
+    value_targets = torch.tensor(value_targets, dtype=torch.float32, device=device)
+
+    out_policy, out_value = model(state)
+
+    if mask_invalid_actions:
+        valid_action_masks = torch.stack(
+            [
+                torch.tensor(s.valid_action_mask, dtype=torch.bool, device=device)
+                for s in game_steps
+            ]
+        )
+        out_policy = out_policy.masked_fill(~valid_action_masks, -1e32)
+
+    policy_loss = F.cross_entropy(out_policy, policy_targets)
+    value_loss = F.mse_loss(out_value, value_targets)
+    loss = policy_loss + value_loss
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return policy_loss.item(), value_loss.item()
+
+
+def board2tensor(board: t3.Board, current_player: t3.Player):
+    np_array = np.array(
+        [
+            [c == current_player for c in board],
+            [c == t3.EMPTY for c in board],
+            [c == t3.other_player(current_player) for c in board],
+        ],
+        dtype=np.float32,
+    ).reshape(3, 3, 3)
+    return torch.from_numpy(np_array)
 
 
 if __name__ == "__main__":
