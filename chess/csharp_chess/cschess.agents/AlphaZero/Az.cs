625e2cf9900a02c7dd6e5f4decf11cd6dac39595
me <me@me.com>
2026-03-01 00:20:47 +1100

    profile az

--- chess/csharp_chess/cschess.agents/AlphaZero/Az.cs
+++ chess/csharp_chess/cschess.agents/AlphaZero/Az.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using TorchSharp.Modules;
 
 namespace cschess.agents.AlphaZero;
@@ -8,27 +9,45 @@ public class Az
 {
     public static void Train()
     {
-        const int numGames = 1;
+        const int numIterations = 1;
+        const int nParallelGames = 4;
         var net = new ResNet(1, 1, CPU);
         var optimiser = new Adam(net.ModelParams());
+        var gameTimes = new List<TimeSpan>();
+        var learnTimes = new List<TimeSpan>();
+        var totalSamples = 0;
 
-        for (var i = 0; i < numGames; i++)
+        var stopwatch = new Stopwatch();
+        for (var i = 0; i < numIterations; i++)
         {
-            IEnumerable<GameSample> samples;
+            List<GameSample> samples;
+            TimeSpan playTime;
             using (no_grad())
             {
+                stopwatch.Start();
                 samples = Player.SelfPlayGames(
                     net,
-                    nGames: 1,
+                    nGames: nParallelGames,
                     nMctsSims: 10,
                     cPuct: 2.0,
                     temperature: 1.25,
                     dirichletAlpha: 0.3,
                     dirichletEpsilon: 0.25
                 ).ToList();
+                playTime = stopwatch.Elapsed;
+                totalSamples += samples.Count;
             }
             var (ploss, vloss) = Learner.UpdateNet(net, optimiser, samples, false);
-            Console.WriteLine($"{ploss}, {vloss}");
+            var learnTime = stopwatch.Elapsed - playTime;
+            gameTimes.Add(playTime);
+            learnTimes.Add(learnTime);
+            Console.WriteLine($"played {nParallelGames} games, {totalSamples} steps in {playTime}");
+            Console.WriteLine($"ploss, vloss: {ploss}, {vloss}");
+            var avgGameTimeS = gameTimes.Select(x => x.TotalSeconds).Average();
+            var avgLearnTimeS = learnTimes.Select(x => x.TotalSeconds).Average();
+            var gamesPerSec = nParallelGames / avgGameTimeS;
+            var playStepsPerSec = totalSamples / avgGameTimeS;
+            Console.WriteLine($"{gamesPerSec} games/sec, {playStepsPerSec} steps/sec, avg learn: {avgLearnTimeS}");
         }
     }
 }
