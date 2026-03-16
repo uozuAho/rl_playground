0b6b0fa7445c3b649aa8deb159a784a5b714e08b
me <me@me.com>
2026-03-16 13:38:11 +1100

    rename, extract

--- chess/csharp_chess/cschess.agents/AlphaZero/AzSelfPlayer.cs
+++ chess/csharp_chess/cschess.agents/AlphaZero/AzSelfPlayer.cs
@@ -1,11 +1,10 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
-using cschess.agents.AlphaZero;
 using cschess.csutils;
 using cschess.game;
 using static TorchSharp.torch;
 
-namespace cschess.agents;
+namespace cschess.agents.AlphaZero;
 
 using MoveProbs = Dictionary<Move, float>;
 
@@ -361,44 +360,6 @@ internal class MctsSimState2
         var newNode = Root;
         newNode.State = CodingAdventureChessGame.FromFen(RootFen);
         return newNode;
-    }
-}
-
-internal class TaskMetrics
-{
-    private readonly string _name;
-    private readonly Stopwatch _sw;
-    private TimeSpan _workStarted;
-    private TimeSpan _workTime;
-    private int _games;
-    private int _states;
-
-    private TaskMetrics(string name, Stopwatch sw)
-    {
-        _name = name;
-        _sw = sw;
-    }
-
-    public static TaskMetrics StartNew(string name)
-    {
-        return new TaskMetrics(name, Stopwatch.StartNew());
-    }
-
-    public void StartWork() => _workStarted = _sw.Elapsed;
-    public void StopWork() => _workTime += _sw.Elapsed - _workStarted;
-    public void IncGame() => _games += 1;
-    public void IncState() => _states += 1;
-    public void IncState(int nStates) => _states += nStates;
-
-    public void PrintSummary()
-    {
-        var totalTime = _sw.Elapsed;
-        var gamesPerSec = _games / totalTime.TotalSeconds;
-        var statesPerSec = _states / totalTime.TotalSeconds;
-        var util = _workTime / totalTime;
-        Console.WriteLine($"{_name}: {_games} games, {_states} states in {totalTime}");
-        Console.WriteLine($"{_name}: {gamesPerSec:F2} games/sec, {statesPerSec:F2} states/sec");
-        Console.WriteLine($"{_name}: utilisation: {util:F2}");
     }
 }
 
