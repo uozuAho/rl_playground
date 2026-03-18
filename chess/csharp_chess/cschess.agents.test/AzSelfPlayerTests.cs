32de0829ea8dace12f434440ec244959d8cbe398
me <me@me.com>
2026-03-18 14:32:13 +1100

    fix tests

--- chess/csharp_chess/cschess.agents.test/AzSelfPlayerTests.cs
+++ chess/csharp_chess/cschess.agents.test/AzSelfPlayerTests.cs
@@ -14,7 +14,8 @@ public class AzSelfPlayerTests
         var net = new ResNet(1, 1, device);
         const int mctsSims = 1;
         const int batchSize = 1;
-        using var tmcts = new AzSelfPlayer(net, mctsSims, batchSize, device, logLevel: LogLevel.None);
+        const int unbatchSize = 1;
+        using var tmcts = new AzSelfPlayer(net, mctsSims, batchSize, unbatchSize, device, logLevel: LogLevel.None);
         var inGame = CodingAdventureChessGame.StandardGame();
         var inFen = inGame.Fen();
         tmcts.Enqueue(inGame);
